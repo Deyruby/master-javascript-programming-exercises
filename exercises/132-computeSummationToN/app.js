3eec8fb2a49c966735ca6f1eabbe6a65319db22b
Deyruby <128061593+Deyruby@users.noreply.github.com>
2024-04-07 01:05:06 +0000

    avances

--- exercises/132-computeSummationToN/app.js
+++ exercises/132-computeSummationToN/app.js
@@ -1,6 +1,11 @@
 function computeSummationToN(n) {
   // your code here
-  
+  let aux = 0
+  for (let i = 1; i < n; i++) {
+    aux += i
+  }
+  return aux
+
 }
 
 let output = computeSummationToN(6);
